02a43b3489865ea7a2b45588381389ca4d498979
bhoodevsharma14 <bhoodev.sharma14@gmail.com>
2020-09-06 19:45:37 +0530

    Results Are Sorted And Stored An Array

--- Arithmetic_Computation_and_sorting.sh
+++ Arithmetic_Computation_and_sorting.sh
@@ -4,6 +4,31 @@ read -p "Enter value of a : " a
 read -p "Enter value of b : " b
 read -p "Enter value of c : " c
 
+function SortArray()
+{
+	newArray=("$@")
+        while True
+        do
+                max=0
+                for index in ${!newArray[@]}
+                do
+								if (( ${max%%.*} == 0 || ${newArray[$index]%%.*} > ${max%%.*} ))
+                        then
+                                max=${newArray[$index]}
+                                remove=$index
+                        fi
+                done
+                SortedArray+=($max)
+                unset newArray[$remove]
+                length=${#newArray[@]}
+                if [ $length -eq 0 ]
+                then
+                        break
+                fi
+        done
+        echo Sorted Array is ${SortedArray[@]}
+}
+
 declare -A Result
 
 # UC-2 Operation
@@ -31,8 +56,10 @@ compute=$(($a%$b+$c))
 echo " a%b+c = $compute"
 Result["UC5"]=$compute
 
+
 for step in ${!Result[@]}
 do
 	ResultArray+=(${Result[$step]})
 done
 
+SortArray ${ResultArray[@]}
